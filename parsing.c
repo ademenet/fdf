535769a38ca0fad7c47d64f3d84935f87a3f1bfe
Alain DEMENET <ademenet@e2r7p15.42.fr>
2016-04-06 19:34:21 +0200

    Creating file to keep things DRY

---
+++ parsing.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/06 17:40:52 by ademenet          #+#    #+#             */
+/*   Updated: 2016/04/06 19:32:04 by ademenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+def ft parse (string)
+	declare line
+	line = GNL
+
+def ft lecture (string line)
+	tant que i != \0
+		si i == ' '
+			i++
+		sinon
+			
+
+COORD	ft_parse_line()
+{
+	COORD	f_list;
+	char	**line;
+	int		gnl;
+
+	line = get_next_line(fd, );
+	return ();
+}
