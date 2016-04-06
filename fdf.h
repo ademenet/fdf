d3d5d9145fa5e878d5828513fc4dc29901469b1a
Alain DEMENET <ademenet@e2r7p15.42.fr>
2016-04-06 19:33:15 +0200

    Normalize and adding libft

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 17:42:15 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/06 18:37:23 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/06 19:19:08 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@
 # include <stdio.h>
 # include <math.h>
 # include "mlx.h"
+# include "libft/libft.h"
 
 # define COORD t_coord
 
-typedef struct	s_coord
+typedef struct s_coord	t_coord;
+
+struct	s_coord
 {
 	int x;
 	int y;
 	int z;
 	COORD *next;
-}				t_coord;
+};
 
 #endif
