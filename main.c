fe38f07da89477446f7f0560103a4e867fbf2a80
Alain DEMENET <ademenet@e2r7p15.42.fr>
2016-04-06 19:33:55 +0200

    Testing get_next_line for parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 17:14:41 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/06 18:55:12 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/06 19:30:22 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 int		main(int argc, char **argv)
 {
 	int		fd;
+	int		gnl;
+	char	*line[100];
 
 	if (argc == 2)
 	{
-		fd = open(argv, O_RDONLY);
+		fd = open(argv[1], O_RDONLY);
+		while ((gnl = get_next_line(fd, line)) != 0)
+		{
+			ft_putstr(*line);
+			ft_putstr("\n");
+		}
 	}
 	return (0);
 }
