47766372fcf59fd15e2ebd673c69acc0ca2594b0
Alain DEMENET <ademenet@e2r7p15.42.fr>
2016-04-07 19:36:18 +0200

    Try hard

--- main.c
+++ main.c
@@ -12,15 +12,48 @@
 
 #include "fdf.h"
 
+int		get_line(char *argv)
+{
+	int fd;
+	char buf[BUFF_SIZE + 1];
+	int lines;
+
+	lines = 0;
+	if (!(fd = open(argv, O_RDONLY)))
+		return(0);
+	while (read(fd, buf, BUFF_SIZE))
+	{
+		while (*buf == '\n')
+			lines++;
+	}
+	return (lines);
+}
+
+int		get_column(char *argv)
+{
+	int		fd;
+	int		column;
+	char	*line;
+
+	column = 0;
+	if (!(fd = open(argv, O_RDONLY)))
+		return(0);
+	get_next_line(fd, &line);
+	column = ft_count_words(line, ' ');
+	return (column);
+}
+
 int		main(int argc, char **argv)
 {
 	int		fd;
-	int		gnl;
-	char	*line[100];
-	COORD	*pixels;
+	int		lines;
+	int		nb_lines_columns[2];
 
 	if (argc == 2)
 	{
+		nb_lines_columns[0] = get_line(argv[1]);
+		nb_lines_columns[1] = get_column(argv[1]);
 	}
+	printf("line = %d\n colonne = %d\n ", nb_lines_columns[0], nb_lines_columns[1]);
 	return (0);
 }
