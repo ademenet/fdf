e4f5a53b622e34fc12f0d8464df5d6d4cedf245b
Alain DEMENET <ademenet@e2r7p14.42.fr>
2016-04-11 14:31:55 +0200

    Ready for merge

--- iso_projection.c
+++ iso_projection.c
@@ -6,32 +6,17 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/10 19:46:13 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 10:12:25 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/11 14:31:22 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-
-u = x*cos(α) + y*cos(α+120°) + z*cos(α-120°)
-v = x*sin(α) + y*sin(α+120°) + z*sin(α-120°)
 
 typedef struct	s_iso
 {
 	int		u;
 	int		z;
 }				t_iso;
-
-/*
- * t_iso	iso_conversion(PXL *pxl,)
- * {
- * 	int		alpha;
- *
- * 	alpha =
- * 	u = PXL->x * cos(alpha) + PXL->y * cos(alpha + (120 * M_PI / 180)) + PXL->z * cos(alpha - (120 * M_PI / 180);
- * 	v = PXL->x * sin(alpha) + PXL->y * sin(alpha + (120 * M_PI / 180)) + PXL->z * sin(alpha - (120 * M_PI / 180);
- * }
- */
 
  t_iso		iso_conversion(PXL *pxl)
  {
