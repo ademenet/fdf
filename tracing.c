a1645db32820b321841d9fa502fce4e5878eb8db
Alain DEMENET <ademenet@e2r7p14.42.fr>
2016-04-11 14:28:35 +0200

    Integrating, but still bugging

--- tracing.c
+++ tracing.c
@@ -6,20 +6,20 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 11:48:58 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 10:51:06 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/11 13:53:40 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	ft_case_2(TRACE trace, FDF data)
+static void	ft_case_2(TRACE trace, FDF *data)
 {
 	int		i;
 
 	i = 0;
 	while (i <= trace.Dy)
 	{
-		mlx_pixel_put(data.mlx, data.win, trace.x1, trace.y1, 0x00FFFFFF);
+		mlx_pixel_put(data->mlx, data->win, trace.x1, trace.y1, 0x00FFFFFF);
 		trace.y1 += trace.y_incr;
 		trace.ey -= trace.dx;
 		if (trace.ey < 0)
@@ -31,14 +31,21 @@ static void	ft_case_2(TRACE trace, FDF data)
 	}
 }
 
-static void	ft_case_1(TRACE trace, FDF data)
+static void	ft_case_1(TRACE trace, FDF *data)
 {
 	int		i;
 
 	i = 0;
 	while (i <= trace.Dx)
 	{
-		mlx_pixel_put(data.mlx, data.win, trace.x1, trace.y1, 0x00FFFFFF);
+		ft_putstr("x1 = ");
+		ft_putnbr(trace.x1);
+		ft_putstr("\n");
+		ft_putstr("y1 = ");
+		ft_putnbr(trace.y1);
+		ft_putstr("\n");
+
+		mlx_pixel_put(data->mlx, data->win, trace.x1, trace.y1, 0x00FFFFFF);
 		trace.x1 += trace.x_incr;
 		trace.ex -= trace.dy;
 		if (trace.ex < 0)
@@ -50,20 +57,20 @@ static void	ft_case_1(TRACE trace, FDF data)
 	}
 }
 
-void		iniatilizing_bresenham(PXL *data)
+void		iniatilizing_bresenham(FDF *data)
 {
 	TRACE	trace;
 	PXL		*pxl1;
 	PXL		*pxl2;
 
+	pxl1 = data->head;
+	pxl2 = data->head->prev;
 	while (pxl2 != NULL)
 	{
-		pxl1 = data;
-		pxl2 = data->next;
-		trace.x1 = pxl1->x;
-		trace.y1 = pxl1->y;
-		trace.x2 = pxl2->x;
-		trace.y2 = pxl2->y;
+		trace.x1 = pxl1->x * 40;
+		trace.y1 = pxl1->y * 40;
+		trace.x2 = pxl2->x * 40;
+		trace.y2 = pxl2->y * 40;
 		trace.ex = abs(pxl1->x - pxl2->x);
 		trace.ey = abs(pxl1->y - pxl2->y);
 		trace.dx = 2 * trace.ex;
@@ -72,15 +79,21 @@ void		iniatilizing_bresenham(PXL *data)
 		trace.Dy = trace.ey;
 		trace.x_incr = 1;
 		trace.y_incr = 1;
-		if (x1 > x2)
+		if (pxl1->x > pxl2->x)
 			trace.x_incr = -1;
-		if (y1 > y2)
+		if (pxl1->y > pxl2->y)
 			trace.y_incr = -1;
 		if (trace.Dx > trace.Dy)
+		{
+			ft_putstr("Dx > Dy\n");
 			ft_case_1(trace, data);
+		}
 		if (trace.Dx < trace.Dy)
+		{
+			ft_putstr("Dx < Dy\n");
 			ft_case_2(trace, data);
+		}
 		pxl1 = pxl2;
-		pxl2 = pxl1->next;
+		pxl2 = pxl1->prev;
 	}
 }
