438d9308fdd7b676ee95284bbc7d3d7387627fab
Alain DEMENET <ademenet@e2r7p14.42.fr>
2016-04-11 10:13:07 +0200

    Another algorithm in order to transform coordinates to iso coordinates

--- iso_projection.c
+++ iso_projection.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/10 19:46:13 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 09:26:09 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/11 10:12:25 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,28 @@
 u = x*cos(α) + y*cos(α+120°) + z*cos(α-120°)
 v = x*sin(α) + y*sin(α+120°) + z*sin(α-120°)
 
-struct	s_iso
+typedef struct	s_iso
 {
 	int		u;
 	int		z;
-};
+}				t_iso;
 
-s_iso	iso_conversion(PXL *pxl)
-{
-	u = PXL->x * cos(M_PI4) + PXL->y * cos(M_PI4) + PXL->z * cos(M_PI4);
-	v = PXL->x * sin(M_PI4) + PXL->y * sin(M_PI4) + PXL->z * sin(M_PI4);
-}
+/*
+ * t_iso	iso_conversion(PXL *pxl,)
+ * {
+ * 	int		alpha;
+ *
+ * 	alpha =
+ * 	u = PXL->x * cos(alpha) + PXL->y * cos(alpha + (120 * M_PI / 180)) + PXL->z * cos(alpha - (120 * M_PI / 180);
+ * 	v = PXL->x * sin(alpha) + PXL->y * sin(alpha + (120 * M_PI / 180)) + PXL->z * sin(alpha - (120 * M_PI / 180);
+ * }
+ */
+
+ t_iso		iso_conversion(PXL *pxl)
+ {
+	t_iso	iso;
+
+	iso->u = sqrt(2) / 2 * (pxl->x - pxl->y);
+	iso->v = sqrt(2 / 3) * pxl->z - 1 / sqrt(6) * (pxl->x + pxl->y);
+	return (iso);
+ }
