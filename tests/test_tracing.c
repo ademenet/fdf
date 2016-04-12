1a531de4ba5c0660b1ca6de36c97085f2134182d
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 17:55:25 +0200

    Update tests

--- tests/test_tracing.c
+++ tests/test_tracing.c
@@ -10,6 +10,7 @@ int		main(int ac, char **av)
 		env.mlx = mlx_init();
 		env.win = mlx_new_window(env.mlx, 1366, 768, "FDF");
 		iso_conversion(env);
+		// z_rotate(env);
 		tracing_lines(env);
 		tracing_columns(env);
 		mlx_loop(env.mlx);
