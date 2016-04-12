3061dc3cd3353e39f7196211788b4d604b9d7ac2
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 11:18:27 +0200

    New parsing algorithm, based on 2D structure and no more on linked list

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 17:42:15 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 18:53:31 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/12 11:17:06 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,20 @@
 # define PXL 	t_pxl
 # define ENV	t_env
 
-typedef struct s_pxl 	t_pxl;
+typedef struct s_pxl	t_pxl;
 typedef struct s_env	t_env;
 
-struct 		s_pxl
+struct		s_pxl
 {
-	int 	x;
-	int 	y;
-	int 	z;
+	int		x;
+	int		y;
+	int		z;
 	char	*color;
-	};
+};
 
 struct		s_env
 {
-	void 	*mlx;
+	void	*mlx;
 	void	*win;
 
 	int		l_nbr;
@@ -55,18 +55,14 @@ struct		s_env
 	int		dy;
 	int		ex;
 	int		ey;
-	int		Dx;
-	int		Dy;
+	int		dx_c;
+	int		dy_c;
 	int		x_incr;
 	int		y_incr;
+
+	PXL		**map;
 };
 
-// FDF			*ft_fill_struct_pxl(FDF *fdf, int fd);
-// PXL			*ft_create_pxl(FDF *fdf, int pxl_info[3]);
-// FDF			*ft_create_fdf(void);
-// PXL			*ft_add_pxl(FDF *fdf, int pxl_info[3]);
-// void		ft_destroy_struct(FDF *fdf);
-// void		iniatilizing_bresenham(FDF *data);
-// ISO			iso_conversion(PXL *pxl);
+ENV			parsing(ENV env, char *av);
 
 #endif
--- parsing.c
+++ parsing.c
@@ -6,103 +6,76 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/06 17:40:52 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 19:14:10 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/12 11:15:02 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/*
- * void	parsing(char *av)
- * {
- *
- * }
- */
-
-void 	counting_lines_and_columns(ENV env, char *av)
+static ENV		getting_size(ENV env, int fd)
 {
-	// char	**tmp;
+	char	**tmp;
 	char	*line;
-	int		fd;
+	int		gnl;
+	int		i;
 
 	env.l_nbr = 0;
 	env.c_nbr = 0;
-	if ((fd = open(av, O_RDONLY)) < 0)
-		perror("Couldn't open file.");
-	while (get_next_line(fd, &line) > 0)
+	while ((gnl = get_next_line(fd, &line)) > 0)
 	{
-		/*
-		 * tmp = ft_strsplit(line, ' ');
-		 * while (tmp == NULL)
-		 * {
-		 * 	env.c_nbr++;
-		 * 	tmp++;
-		 * }
-		 * free(tmp);
-		 */
+		if (env.l_nbr == 0)
+		{
+			i = 0;
+			tmp = ft_strsplit(line, ' ');
+			while (tmp[i] != NULL)
+			{
+				env.c_nbr++;
+				i++;
+			}
+			free(tmp);
+		}
 		env.l_nbr++;
 	}
-	close(fd);
+	return (env);
 }
 
-int		main(int ac, char **av)
+static ENV		getting_content(ENV env, int fd)
 {
-	ENV		env;
+	char	**tmp;
+	char	*line;
+	int		i;
+	int		j;
 
-	if (ac == 2)
+	i = 0;
+	env.map = (PXL**)malloc(sizeof(PXL*) * env.l_nbr);
+	while (get_next_line(fd, &line) > 0)
 	{
-		counting_lines_and_columns(env, av[1]);
-		printf("line %d\ncol %d\n", env.l_nbr, env.c_nbr);
+		tmp = ft_strsplit(line, ' ');
+		env.map[i] = (PXL*)malloc(sizeof(PXL) * env.c_nbr);
+		j = 0;
+		while (tmp[j] != NULL)
+		{
+			env.map[i][j].x = j + 1;
+			env.map[i][j].y = i + 1;
+			env.map[i][j].z = ft_atoi(tmp[j]);
+			j++;
+		}
+		i++;
 	}
-	return (0);
+	return (env);
 }
 
-/*
- * PXL		*ft_fill_struct_pxl(FDF *fdf, int fd)
- * {
- * 	char		*line;
- * 	int			pxl_info[3];
- * 	char		**tmp;
- * 	char		**tmp2 = NULL;
- * 	char		*color;
- *
- * 	pxl_info[0] = 0;
- * 	pxl_info[1] = 0;
- * 	while ((pxl_info[0] = get_next_line(fd, &line)))
- * 	{
- * 		tmp = ft_strsplit(line, ' ');
- * 		while (tmp[pxl_info[0]])
- * 		{
- * 			if (ft_strrchr(*tmp, ',') != NULL && tmp2 == ft_strsplit(*tmp, ','))
- * 			{
- * 				pxl_info[2] = ft_atoi(tmp2[0]);
- * 				color = tmp2[1];
- * 			}
- * 			pxl_info[2] = ft_atoi(tmp[pxl_info[0]]);
- * 			fdf->head = ft_add_pxl(fdf, pxl_info);
- * 			pxl_info[0]++;
- * 		}
- * 		pxl_info[1]++;
- * 	}
- * 	return (fdf);
- * }
- *
- * FDF				*ft_print_pxl(FDF *fdf)
- * {
- * 	int		space;
- * 	PXL		**pixels;
- *
- * 	space = 10;
- * 	while (fdf->head->prev)
- * 	{
- * 		if (fdf->head->z == 0)
- * 			mlx_pixel_put(fdf->mlx, fdf->win, (fdf->head->x * space),
- * 				(fdf->head->y * space), 0xFFFFFF);
- * 		else
- * 			mlx_pixel_put(fdf->mlx, fdf->win, (fdf->head->x * space),
- * 				(fdf->head->y * space), 0x0000FF);
- * 		fdf->head = fdf->head->prev;
- * 	}
- * 	return (fdf);
- * }
- */
+ENV				parsing(ENV env, char *av)
+{
+	int		fd;
+
+	if ((fd = open(av, O_RDONLY)) < 0)
+		perror("error: couldn't open file.");
+	env = getting_size(env, fd);
+	close(fd);
+	if ((fd = open(av, O_RDONLY)) < 0)
+		perror("error: couldn't open file.");
+	env = getting_content(env, fd);
+	close(fd);
+	return (env);
+}
