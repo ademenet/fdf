07f6a72a249b719b00d9f45720afd9b4027dc1a6
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 11:43:25 +0200

    Adding UT for iso_projection

---
+++ tests/test_iso.c
@@ -0,0 +1,25 @@
+#include "../fdf.h"
+
+int		main(int ac, char **av)
+{
+	ENV		env;
+
+	if (ac == 2)
+	{
+		env = parsing(env, av[1]);
+		ft_putnbr(env.map[3][2].x);
+		ft_putstr("\n");
+		ft_putnbr(env.map[3][2].y);
+		ft_putstr("\n");
+		env = iso_conversion(env);
+		ft_putnbr(env.l_nbr);
+		ft_putstr("\n");
+		ft_putnbr(env.c_nbr);
+		ft_putstr("\n");
+		ft_putnbr(env.map[3][2].x);
+		ft_putstr("\n");
+		ft_putnbr(env.map[3][2].y);
+		ft_putstr("\n");
+	}
+	return (0);
+}
