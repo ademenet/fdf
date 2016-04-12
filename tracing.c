0b4b03bb38b867ee31fec8013400a9f355857412
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 16:07:05 +0200

    Tracing is ok now

--- tracing.c
+++ tracing.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 11:48:58 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/12 14:29:32 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/12 16:03:46 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,40 @@ void		tracing_lines(ENV env)
 			y1 = env.map[y][x].y * 40;
 			x2 = env.map[y][x + 1].x * 40;
 			y2 = env.map[y][x + 1].y * 40;
+			env.ex = abs(x1 - x2);
+			env.ey = abs(y1 - y2);
+			env.x_incr = 1;
+			env.y_incr = 1;
+			if (x1 > x2)
+				env.x_incr = -1;
+			if (y1 > y2)
+				env.y_incr = -1;
+			iniatilizing_tracing(env, x1, y1, x2, y2);
+			x++;
+		}
+		y++;
+	}
+}
+
+void		tracing_columns(ENV env)
+{
+	int		x;
+	int		y;
+	int		x1;
+	int		y1;
+	int		x2;
+	int		y2;
+
+	y = 0;
+	while (y < env.l_nbr - 1)
+	{
+		x = 0;
+		while (x < env.c_nbr)
+		{
+			x1 = env.map[y][x].x * 40;
+			y1 = env.map[y][x].y * 40;
+			x2 = env.map[y + 1][x].x * 40;
+			y2 = env.map[y + 1][x].y * 40;
 			env.ex = abs(x1 - x2);
 			env.ey = abs(y1 - y2);
 			env.x_incr = 1;
