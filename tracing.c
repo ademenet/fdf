86f30e8eae90b07a299a24642317b57c0798c9b4
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 13:42:50 +0200

    Save

--- tracing.c
+++ tracing.c
@@ -6,86 +6,88 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 11:48:58 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/11 16:16:50 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/12 13:42:30 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	ft_case_2(TRACE trace, FDF *data)
+static void	ft_case_2(ENV env)
 {
 	int		i;
 
 	i = 0;
-	while (i <= trace.Dy)
+	while (i <= env.dy_c)
 	{
-		mlx_pixel_put(data->mlx, data->win, trace.x1, trace.y1, 0x00FFFFFF);
-		trace.y1 += trace.y_incr;
-		trace.ey -= trace.dx;
-		if (trace.ey < 0)
+		mlx_pixel_put(data->mlx, data->win, env.x1, env.y1, 0x00FFFFFF);
+		env.y1 += env.y_incr;
+		env.ey -= env.dx;
+		if (env.ey < 0)
 		{
-			trace.x1 += trace.x_incr;
-			trace.ey += trace.dy;
+			env.x1 += env.x_incr;
+			env.ey += env.dy;
 		}
 		i++;
 	}
 }
 
-static void	ft_case_1(TRACE trace, FDF *data)
+static void	ft_case_1(ENV env)
 {
 	int		i;
 
 	i = 0;
-	while (i <= trace.Dx)
+	while (i <= env.dx_c)
 	{
-
-		mlx_pixel_put(data->mlx, data->win, trace.x1, trace.y1, 0x00FFFFFF);
-		trace.x1 += trace.x_incr;
-		trace.ex -= trace.dy;
-		if (trace.ex < 0)
+		mlx_pixel_put(env.mlx, env.win, env.map[y][x].x, env.map[y][x].y, 0x00FFFFFF);
+		env.map[y][x].x += env.x_incr;
+		env.ex -= env.dy;
+		if (env.ex < 0)
 		{
-			trace.y1 += trace.y_incr;
-			trace.ex += trace.dx;
+			env.map[y][x].y += env.y_incr;
+			env.ex += env.dx;
 		}
 		i++;
 	}
 }
 
-void		iniatilizing_bresenham(ISO pxl_1, ISO pxl_2)
+void		iniatilizing_tracing(ENV env)
 {
-	TRACE	trace;
-	PXL		*pxl1;
-	PXL		*pxl2;
-
-	pxl1 = data->head;
-	pxl2 = data->head->prev;
-	while (pxl2 != NULL)
+	env.ex = abs(env.map[y][x].x - env.map[y][x + 1].x);
+	env.ey = abs(env.map[y][x].y - env.map[y][x + 1].y);
+	env.dx = 2 * env.ex;
+	env.dy = 2 * env.ey;
+	env.dx_c = env.ex;
+	env.dy_c = env.ey;
+	env.x_incr = 1;
+	env.y_incr = 1;
+	if (env.map[y][x].x > env.map[y][x + 1].x)
+		env.x_incr = -1;
+	if (env.map[y][x].y > env.map[y][x + 1].y)
+		env.y_incr = -1;
+	if (env.dx_c > env.dy_c)
 	{
-		trace.x1 = pxl1->x * 40;
-		trace.y1 = pxl1->y * 40;
-		trace.x2 = pxl2->x * 40;
-		trace.y2 = pxl2->y * 40;
-		trace.ex = abs(trace.x1 - trace.x2);
-		trace.ey = abs(trace.y1 - trace.y2);
-		trace.dx = 2 * trace.ex;
-		trace.dy = 2 * trace.ey;
-		trace.Dx = trace.ex;
-		trace.Dy = trace.ey;
-		trace.x_incr = 1;
-		trace.y_incr = 1;
-		if (pxl1->x > pxl2->x)
-			trace.x_incr = -1;
-		if (pxl1->y > pxl2->y)
-			trace.y_incr = -1;
-		if (trace.Dx > trace.Dy)
+		ft_case_1(env, int x, int y);
+	}
+	if (env.dx_c < env.dy_c)
+	{
+		ft_case_2(env, int x, int y);
+	}
+}
+
+void		tracing_lines(ENV env)
+{
+	int		x;
+	int		y;
+
+	y = 0;
+	while (y < env.l_nbr)
+	{
+		x = 0;
+		while (x < env.c_nbr - 1)
 		{
-			ft_case_1(trace, data);
+			
+			x++;
 		}
-		if (trace.Dx < trace.Dy)
-		{
-			ft_case_2(trace, data);
-		}
-		pxl1 = pxl2;
-		pxl2 = pxl1->prev;
+		y++;
 	}
 }
