707ff36472e78b890bc352a1c978d2a961b27eaa
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 16:07:25 +0200

    Tracing test file

---
+++ tests/test_tracing.c
@@ -0,0 +1,18 @@
+#include "../fdf.h"
+
+int		main(int ac, char **av)
+{
+	ENV		env;
+
+	if (ac == 2)
+	{
+		env = parsing(env, av[1]);
+		env.mlx = mlx_init();
+		env.win = mlx_new_window(env.mlx, 1366, 768, "FDF");
+		iso_conversion(env);
+		tracing_lines(env);
+		tracing_columns(env);
+		mlx_loop(env.mlx);
+	}
+	return (0);
+}
