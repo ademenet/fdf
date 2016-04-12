5e75f8b7e78c4089d7a5fd1ff4338294e277919c
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 14:32:56 +0200

    Trying to find solutions, it works, need refact

--- tracing.c
+++ tracing.c
@@ -6,51 +6,51 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 11:48:58 by ademenet          #+#    #+#             */
-/*   Updated: 2016/04/12 14:21:10 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/04/12 14:29:32 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	ft_case_2(ENV env, int x, int y)
+static void	ft_case_2(ENV env, int x1, int y1, int x2, int y2)
 {
 	int		i;
 
 	i = 0;
 	while (i <= env.dy_c)
 	{
-		mlx_pixel_put(env.mlx, env.win, env.map[y][x].x, env.map[y][x].y, 0x00FFFFFF);
-		env.map[y][x].y += env.y_incr;
+		mlx_pixel_put(env.mlx, env.win, x1, y1, 0x00FFFFFF);
+		y1 += env.y_incr;
 		env.ey -= env.dx;
 		if (env.ey < 0)
 		{
-			env.map[y][x].x += env.x_incr;
+			x1 += env.x_incr;
 			env.ey += env.dy;
 		}
 		i++;
 	}
 }
 
-static void	ft_case_1(ENV env, int x, int y)
+static void	ft_case_1(ENV env, int x1, int y1, int x2, int y2)
 {
 	int		i;
 
 	i = 0;
 	while (i <= env.dx_c)
 	{
-		mlx_pixel_put(env.mlx, env.win, env.map[y][x].x, env.map[y][x].y, 0x00FFFFFF);
-		env.map[y][x].x += env.x_incr;
+		mlx_pixel_put(env.mlx, env.win, x1, y1, 0x00FFFFFF);
+		x1 += env.x_incr;
 		env.ex -= env.dy;
 		if (env.ex < 0)
 		{
-			env.map[y][x].y += env.y_incr;
+			y1 += env.y_incr;
 			env.ex += env.dx;
 		}
 		i++;
 	}
 }
 
-static void		iniatilizing_tracing(ENV env, int x, int y)
+static void		iniatilizing_tracing(ENV env, int x1, int y1, int x2, int y2)
 {
 	env.dx = 2 * env.ex;
 	env.dy = 2 * env.ey;
@@ -58,11 +58,11 @@ static void		iniatilizing_tracing(ENV env, int x, int y)
 	env.dy_c = env.ey;
 	if (env.dx_c > env.dy_c)
 	{
-		ft_case_1(env, x, y);
+		ft_case_1(env, x1, y1, x2, y2);
 	}
 	if (env.dx_c < env.dy_c)
 	{
-		ft_case_2(env, x, y);
+		ft_case_2(env, x1, y1, x2, y2);
 	}
 }
 
@@ -74,22 +74,26 @@ void		tracing_lines(ENV env)
 	int		y1;
 	int		x2;
 	int		y2;
-	
+
 	y = 0;
 	while (y < env.l_nbr)
 	{
 		x = 0;
 		while (x < env.c_nbr - 1)
 		{
-			env.ex = abs(env.map[y][x].x - env.map[y][x + 1].x);
-			env.ey = abs(env.map[y][x].y - env.map[y][x + 1].y);
+			x1 = env.map[y][x].x * 40;
+			y1 = env.map[y][x].y * 40;
+			x2 = env.map[y][x + 1].x * 40;
+			y2 = env.map[y][x + 1].y * 40;
+			env.ex = abs(x1 - x2);
+			env.ey = abs(y1 - y2);
 			env.x_incr = 1;
 			env.y_incr = 1;
-			if (env.map[y][x].x > env.map[y][x + 1].x)
+			if (x1 > x2)
 				env.x_incr = -1;
-			if (env.map[y][x].y > env.map[y][x + 1].y)
+			if (y1 > y2)
 				env.y_incr = -1;
-			iniatilizing_tracing(env, x, y);
+			iniatilizing_tracing(env, x1, y1, x2, y2);
 			x++;
 		}
 		y++;
