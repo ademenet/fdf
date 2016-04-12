f47b3cf053d27c28e0d5c8cdeeeeb525147f0d2f
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-04-12 11:17:37 +0200

    Building some unit tests

---
+++ tests/test_parsing.c
@@ -0,0 +1,16 @@
+int		main(int ac, char **av)
+{
+	ENV		env;
+
+	if (ac == 2)
+	{
+		env = parsing(env, av[1]);
+		ft_putnbr(env.l_nbr);
+		ft_putstr("\n");
+		ft_putnbr(env.c_nbr);
+		ft_putstr("\n");
+		ft_putnbr(env.map[3][2].z);
+		ft_putstr("\n");
+	}
+	return (0);
+}
